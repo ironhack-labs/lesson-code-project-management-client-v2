a3d8aacb92bdfa0b578ffea369ad121a7c886c76
ross-u <cirkovic.uros.srb@gmail.com>
2021-08-04 21:12:57 +0200

    Example 12 - Pre-populate the edit from fields

--- src/pages/EditProjectPage.js
+++ src/pages/EditProjectPage.js
@@ -6,7 +6,32 @@ const API_URL = "http://localhost:5000";
 function EditProjectPage(props) {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+
+  // We access the URL parameter `:id` and save it in a variable
+  const projectId = props.match.params.id;											 // <== ADD
   
+ 
+ // This effect will run after the initial render 
+ // and each time the project id coming from
+ // the URL parameter `props.match.params.id` changes.
+  useEffect(() => {                                             // <== ADD
+    axios
+      .get(`${API_URL}/api/projects/${projectId}`)
+      .then((response) => {
+        /* 
+          We update the state with the project data coming from the repsonse.
+          This way our inputs will show the actual title and description 
+         of the project during the edit.
+        */
+        const oneProject = response.data;
+        setTitle(oneProject.title);
+        setDescription(oneProject.description);
+      })
+      .catch((error) => console.log(error));
+    
+  }, [projectId]);
+  
+
   return (
     <div className="EditProjectPage">
       <h3>Edit the Project</h3>
