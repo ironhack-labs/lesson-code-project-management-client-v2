a881fe65b40cb680083a9f549861db81d5e97a23
ross-u <cirkovic.uros.srb@gmail.com>
2021-08-04 21:17:22 +0200

    Example 13 - Update EditProjectPage component - DELETE request

--- src/pages/EditProjectPage.js
+++ src/pages/EditProjectPage.js
@@ -21,27 +21,36 @@ function EditProjectPage(props) {
   }, [projectId]);
   
 
-  const handleFormSubmit = (e) => {                                 // <== ADD
+  const handleFormSubmit = (e) => {
     e.preventDefault();
-    // Create an object representing the body of the PUT request
     const requestBody = { title, description };
 
-    // Make a PUT request to update the project
     axios
       .put(`${API_URL}/api/projects/${projectId}`, requestBody)
       .then((response) => {
-        // Once the request is resolved successfuly and the project
-        // is updated we navigate back to the details page
         props.history.push(`/projects/${projectId}`)
       });
-  };  
+  };
   
+  
+  const deleteProject = () => {                               //  <== ADD
+    // Make a DELETE request to delete the project
+    axios
+      .delete(`${API_URL}/api/projects/${projectId}`)
+      .then(() => {
+        // Once the request is resolved successfully and the project
+        // is deleted navigate back to the list of projects.
+        props.history.push("/projects");
+      })
+      .catch((err) => console.log(err));
+  };  
 
+  
   return (
     <div className="EditProjectPage">
       <h3>Edit the Project</h3>
 
-      <form onSubmit={handleFormSubmit}>                      {/*  <== UPDATE  */}
+      <form onSubmit={handleFormSubmit}>
         <label>Title:</label>
         <input
           type="text"
@@ -57,8 +66,10 @@ function EditProjectPage(props) {
           onChange={(e) => setDescription(e.target.value)}
         />
 
-        <input type="submit" value="Submit" />
+        <button type="submit">Update Project</button>
       </form>
+
+      <button onClick={deleteProject}>Delete Project</button>       {/* <== ADD  */}      
     </div>
   );
 }
