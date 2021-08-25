c58e114dea021c4fb696e0e40584e7d49c13822d
sandrabosk <AB@MacBook-Pro.local>
2021-08-25 05:11:41 +0200

    update server port

--- src/components/AddProject.js
+++ src/components/AddProject.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 
-const API_URL = "http://localhost:5000";
+const API_URL = "http://localhost:5005";
 
 function AddProject(props) {
   const [title, setTitle] = useState("");
